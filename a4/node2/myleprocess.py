73c655492e4faf4091684fd1abe245ffacc8413b
Swan Pyae Sone Tun <swanpyaesonetun@swanpyaes-mac.local>
2025-07-19 22:41:44 -0700

    added a4

--- a4/node2/myleprocess.py
+++ a4/node2/myleprocess.py
@@ -4,73 +4,72 @@ import uuid
 import json
 import time
 
-
 class Message:
-    def __init__(self, uid, flag):
-        self.uuid = str(uid)
+    def __init__(self, uuid_str, flag):
+        self.uuid = uuid_str
         self.flag = flag
 
     def to_json(self):
         return json.dumps(self.__dict__) + "\n"
 
     @staticmethod
-    def from_json(json_str):
-        data = json.loads(json_str)
-        msg = Message(data['uuid'], data['flag'])
-        return msg
-
-
-def log_event(log_file, direction, msg, comparison=None, state=None):
-    with open(log_file, 'a') as f:
-        f.write(f"{direction}: uuid={msg.uuid}, flag={msg.flag}" +
-                (f", {comparison}, {state}" if comparison else "") + "\n")
-
+    def from_json(data):
+        obj = json.loads(data)
+        return Message(obj['uuid'], obj['flag'])
 
 class Node:
-    def __init__(self, config_path, log_path):
-        self.uuid = uuid.uuid4()
+    def __init__(self, config_file, log_file):
+        self.uuid = str(uuid.uuid4())
         self.leader_id = None
-        self.flag = 0  # 0: still electing, 1: leader elected
-        self.config_path = config_path
-        self.log_path = log_path
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.state = 0  # 0: electing, 1: elected
+        self.log_file = log_file
+        self.server_socket = None
         self.client_socket = None
         self.neighbor_socket = None
+        self.forwarded_leader = False
 
-        self.load_config()
 
-    def load_config(self):
-        with open(self.config_path, 'r') as f:
-            lines = f.readlines()
-            self.my_ip, self.my_port = lines[0].strip().split(',')
-            self.neighbor_ip, self.neighbor_port = lines[1].strip().split(',')
+        with open(config_file, 'r') as f:
+            lines = f.read().splitlines()
+            self.my_ip, self.my_port = lines[0].split(',')
+            self.peer_ip, self.peer_port = lines[1].split(',')
             self.my_port = int(self.my_port)
-            self.neighbor_port = int(self.neighbor_port)
+            self.peer_port = int(self.peer_port)
+
+    def log(self, message):
+        with open(self.log_file, 'a') as f:
+            f.write(message + "\n")
+        print(message)
 
     def start_server(self):
-        self.server_socket.bind((self.my_ip, self.my_port))
-        self.server_socket.listen(1)
-        self.neighbor_socket, _ = self.server_socket.accept()
-        threading.Thread(target=self.receive_messages, daemon=True).start()
+        def server_thread():
+            self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.server_socket.bind((self.my_ip, self.my_port))
+            self.server_socket.listen(1)
+            self.neighbor_socket, _ = self.server_socket.accept()
+            self.receive_messages()
+
+        threading.Thread(target=server_thread, daemon=True).start()
 
     def start_client(self):
         while True:
             try:
-                self.client_socket = socket.socket(
-                    socket.AF_INET, socket.SOCK_STREAM)
-                self.client_socket.connect(
-                    (self.neighbor_ip, self.neighbor_port))
+                self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.client_socket.connect((self.peer_ip, self.peer_port))
                 break
             except:
                 time.sleep(1)
 
-        # Send initial message
+        # send initial message
         msg = Message(self.uuid, 0)
         self.send_message(msg)
 
     def send_message(self, msg):
-        log_event(self.log_path, "Sent", msg)
+        while self.client_socket is None:
+            time.sleep(0.5)  # Wait for connection to be established
         self.client_socket.sendall(msg.to_json().encode())
+        self.log(f"Sent: uuid={msg.uuid}, flag={msg.flag}")
 
     def receive_messages(self):
         buffer = ""
@@ -85,44 +84,48 @@ class Node:
                 self.handle_message(msg)
 
     def handle_message(self, msg):
-        comparison = "greater" if msg.uuid > str(
-            self.uuid) else "less" if msg.uuid < str(self.uuid) else "same"
-        log_event(self.log_path, "Received", msg, comparison, self.flag)
+        comparison = "same" if msg.uuid == self.uuid else ("greater" if msg.uuid > self.uuid else "less")
 
         if msg.flag == 1:
+            self.state = 1
             self.leader_id = msg.uuid
-            self.flag = 1
-            if self.leader_id == str(self.uuid):
-                print(f"leader is {self.leader_id}")
-            else:
+            self.log(f"Received: uuid={msg.uuid}, flag=1, {comparison}, 1")
+
+            if not self.forwarded_leader:
                 self.send_message(msg)
+                self.forwarded_leader = True
+            elif msg.uuid == self.uuid:
+                self.log("Termination condition met. Stopping forwarding.")
             return
 
-        if msg.uuid == str(self.uuid):
-            self.flag = 1
-            self.leader_id = str(self.uuid)
-            print(f"leader is {self.leader_id}")
+        if self.state == 1:
+            self.log(f"Ignored message from uuid={msg.uuid} as already elected")
+            return
+
+        self.log(f"Received: uuid={msg.uuid}, flag=0, {comparison}, 0")
+
+        if msg.uuid > self.uuid:
+            self.send_message(msg)
+        elif msg.uuid == self.uuid:
+            self.state = 1
+            self.leader_id = self.uuid
+            self.log(f"Leader is decided to {self.leader_id}.")
             new_msg = Message(self.uuid, 1)
             self.send_message(new_msg)
-        elif msg.uuid > str(self.uuid):
-            self.send_message(msg)
-        else:
-            with open(self.log_path, 'a') as f:
-                f.write("Ignored message\n")
+        # else: don't forward smaller UUIDs
 
     def run(self):
-        threading.Thread(target=self.start_server, daemon=True).start()
-        time.sleep(2)
+        self.start_server()
+        time.sleep(2)  # wait for servers to be ready
         self.start_client()
 
-        # Keep main thread alive
-        while True:
+        while self.state == 0:
             time.sleep(1)
+        self.log(f"Final Leader: {self.leader_id}")
 
+if __name__ == '__main__':
+    config_file = "config.txt"
+    log_file = "log.txt"
 
-if __name__ == "__main__":
-    import sys
-    config_file = sys.argv[1] if len(sys.argv) > 1 else "config.txt"
-    log_file = sys.argv[2] if len(sys.argv) > 2 else "log.txt"
     node = Node(config_file, log_file)
     node.run()
